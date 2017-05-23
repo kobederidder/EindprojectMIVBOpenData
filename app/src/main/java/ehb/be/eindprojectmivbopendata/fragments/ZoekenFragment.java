ffac35736be97b0a8540f4e64c14ee710c7e3e51
rjh2018 <jelle.hoekstra.2018@hotmail.com>
2017-05-23 14:22:49 +0200

    Jelle : lay-out actionbar aan het opmaken, logo mivb in verwerkt

--- app/src/main/java/ehb/be/eindprojectmivbopendata/fragments/ZoekenFragment.java
+++ app/src/main/java/ehb/be/eindprojectmivbopendata/fragments/ZoekenFragment.java
@@ -2,9 +2,9 @@ package ehb.be.eindprojectmivbopendata.fragments;
 
 import android.app.DatePickerDialog;
 import android.app.Fragment;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,16 +13,20 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
+import java.lang.reflect.Array;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+import java.util.SimpleTimeZone;
 
+import ehb.be.eindprojectmivbopendata.parsers.StopParser;
 import ehb.be.eindprojectmivbopendata.R;
 import ehb.be.eindprojectmivbopendata.source.Stop;
-import ehb.be.eindprojectmivbopendata.util.DatabaseDAO;
-
 
 /**
   Created by mobapp06 on 16/05/17.
@@ -32,18 +36,15 @@ import ehb.be.eindprojectmivbopendata.util.DatabaseDAO;
 public class ZoekenFragment extends Fragment{
 
     private Calendar mCalendar;
-    private EditText etDatum, etUur;
+    private TimePicker mTimePicker;
     private AutoCompleteTextView actvBestemming, actvVertrek;
-    private Button btnZoeken;
+    private Button btnZoeken, btnDatumVertrek, btnUurVertrek;
 
     private String datum = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
     private String uur = new SimpleDateFormat("hh:mm").format(new Date());
     //TO DO - UUR FORMATEREN NAAR EUROPESE WEERGAVE (iets met functie 'LOCALE' ? )
 
-    private final String FRAGMENT_BACKSTACK = "fragment_backstack";
-
-    ArrayList<Stop> haltes;
-    DatabaseDAO dao;
+    ArrayList<Stop> haltes = StopParser.getInstance().mStopList;
 
     public ZoekenFragment() {
     }
@@ -54,63 +55,40 @@ public class ZoekenFragment extends Fragment{
     }
 
     public void geefVertrekHalte(){
-        dao = new DatabaseDAO(getActivity());
-        haltes = dao.getDistinctStopNames();
-        Log.d("AUTOCOMPLETE", String.valueOf(haltes));
-
-        Log.d("AUTOCOMPLETE","build autocomplete");
-        ArrayList<String> halteNamen = new ArrayList<>();
-        for(Stop temp : haltes){
-            halteNamen.add(temp.getStop_name().replace("\"",""));
-            Log.d("AUTOCOMPLETE",temp.getStop_name());
-        }
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>
-                (getActivity(), android.R.layout.select_dialog_item, halteNamen);
+        ArrayAdapter<Stop> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_dropdown_item_1line, haltes);
         actvVertrek.setAdapter(adapter);
-        actvVertrek.setThreshold(1);
-        //actvVertrek.enoughToFilter();
-        //actvVertrek.getDropDownAnchor();
-
-
-        //Stop temp = haltes.get(position);
-
-        //actvVertrek.getText(temp.getStop_name().replace("\"", ""));
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.content_main, container, false);
 
         actvBestemming = (AutoCompleteTextView) rootView.findViewById(R.id.et_bestemming);
         actvVertrek = (AutoCompleteTextView) rootView.findViewById(R.id.et_vertrek);
 
-        etDatum =(EditText) rootView.findViewById(R.id.et_datum);
-        etDatum.setText(datum);
-        etDatum.setOnClickListener(new View.OnClickListener() {
+        btnDatumVertrek =(Button) rootView.findViewById(R.id.btn_datum);
+        btnUurVertrek = (Button) rootView.findViewById(R.id.btn_uur);
+        btnZoeken = (Button) rootView.findViewById(R.id.btn_zoeken);
+
+        btnDatumVertrek.setText(datum);
+        btnDatumVertrek.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 createDatePicker();
             }
         });
 
-        etUur = (EditText) rootView.findViewById(R.id.et_uur);
-        etUur.setText(uur);
-
-        btnZoeken = (Button) rootView.findViewById(R.id.btn_zoeken);
-
-        btnZoeken.setOnClickListener(new View.OnClickListener() {
+        btnUurVertrek.setText(uur);
+        btnUurVertrek.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                getActivity().getFragmentManager().beginTransaction()
-                        .replace(R.id.container, RouteListFragment.newInstance())
-                        .addToBackStack(FRAGMENT_BACKSTACK)
-                        .commit();
+            public void onClick(View view) {
+              //createTimePicker();
             }
         });
 
-        geefVertrekHalte();
+
 
         return rootView;
     }
@@ -128,20 +106,35 @@ public class ZoekenFragment extends Fragment{
                 mCalendar.set(Calendar.YEAR, year);
                 mCalendar.set(Calendar.MONTH, month);
                 mCalendar.set(Calendar.DAY_OF_MONTH, day);
-                etDatum.setText(calendarToString(mCalendar));
+                btnDatumVertrek.setText(calendarToString(mCalendar));
             }
         };
-        //aanmaken dialoogvenster (popup) met datepicker
-        //waar weergeeven? this -> deze activity zelf
-        //verwijzing naar jaar, maand en dag uit mCalendar object
+        //aanmaken dialoogvenster (popup) met datepicker , verwijzing naar jaar, maand en dag uit mCalendar object
         DatePickerDialog dpd = new DatePickerDialog(getActivity(), onDateSetListener
                 , mCalendar.get(Calendar.YEAR)
                 , mCalendar.get(Calendar.MONTH)
                 , mCalendar.get(Calendar.DAY_OF_MONTH));
-
         //dialoogvenster weergeven
         dpd.show();
     }
+/*
+    //toon timePicker om uur te selecteren (hiermee is invoer ook meteen beveiligd)
+    private void createTimePicker() {
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener(){
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                mCalendar.set(Calendar.HOUR, hourOfDay);
+                mCalendar.set(Calendar.MINUTE, minute);
+                btnUurVertrek.setText(timeToString(mCalendar));
+            }
+        };
+        TimePickerDialog tpd = new TimePickerDialog(getActivity(), onTimeSetListener
+            , mCalendar.get(Calendar.HOUR)
+            , mCalendar.get(Calendar.MINUTE));
+
+        tdp.show();
+    }
+*/
 
     private String calendarToString(Calendar calendar)
     {
@@ -149,16 +142,10 @@ public class ZoekenFragment extends Fragment{
         return sdf.format(calendar.getTime());
     }
 
-    View.OnClickListener zoeken = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-            getActivity().getFragmentManager().beginTransaction()
-                    .replace(R.id.container, RouteListFragment.newInstance())
-                    .addToBackStack(FRAGMENT_BACKSTACK)
-                    .commit();
-        }
-    };
-
+    private String timeToString(Calendar calendar)
+    {
+        SimpleDateFormat sdf = new SimpleDateFormat("hh:mm");
+        return sdf.format(calendar.getTime());
+    }
 
 }
